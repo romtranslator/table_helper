12caba88e028fcdc827b37d12880864bb8970d6c
Matthew LaFrance <github@mattsmessyroom.com>
2023-09-18 19:01:01 -0400

    Initial commit of files for table helper.

---
+++ library.php
@@ -0,0 +1,132 @@
+﻿<?php
+
+//takes a path to a charset and returns an array of values
+function get_charset($charset) {
+	//open file
+	$handle = fopen($charset, "r");
+	//read contents
+	$contents = fread($handle, filesize($charset));
+	//close file
+	fclose($handle);
+	
+	//split at line breaks
+	$contents = mb_split ("
+" , $contents);
+	
+	//return results
+	return $contents;
+}
+
+function print_table($char, $hex, $dakuten = null, $handakuten = null, $pre = null) {
+	
+	//pick the charset in which the $char input appears
+	if (in_array($char, $GLOBALS["numerals"])) {
+		$charset = $GLOBALS["numerals"];
+	} elseif (in_array($char, $GLOBALS["english_upper"])) {
+		$charset = $GLOBALS["english_upper"];
+	} elseif (in_array($char, $GLOBALS["english_lower"])) {
+		$charset = $GLOBALS["english_lower"];
+	} elseif (in_array($char, $GLOBALS["hiragana"])) {
+		$charset = $GLOBALS["hiragana"];
+	} elseif (in_array($char, $GLOBALS["hiragana_small"])) {
+		$charset = $GLOBALS["hiragana_small"];
+	} elseif (in_array($char, $GLOBALS["hiragana_diacritic"])) {
+		$charset = $GLOBALS["hiragana_diacritic"];
+	} elseif (in_array($char, $GLOBALS["katakana"])) {
+		$charset = $GLOBALS["katakana"];
+	} elseif (in_array($char, $GLOBALS["katakana_small"])) {
+		$charset = $GLOBALS["katakana_small"];
+	} elseif (in_array($char, $GLOBALS["katakana_diacritic"])) {
+		$charset = $GLOBALS["katakana_diacritic"];
+	} elseif (in_array($char, $GLOBALS["current_game"])) {
+		$charset = $GLOBALS["current_game"];
+	}
+	
+	//if a charset is found
+	if (isset($charset)) {
+		
+		//set position of char in charset
+		$pos = array_search($char, $charset);
+		//get size of charset
+		$count = count($charset);
+		
+		//switch hex value to dec for calculation
+		$hex = hexdec($hex);
+		
+		//iterate through the charset
+		for ($x = 0; $x < ($count-1); $x++) {
+			//print the hex/char pair, calculating
+			//hex  for based on the supplied value		
+			
+			$byte = ($hex - ($pos - $x));
+			
+			$dakuten_chars = array(
+				"か" => "が",
+				"き" => "ぎ",
+				"く" => "ぐ",
+				"け" => "げ",
+				"こ" => "ご",
+				"さ" => "ざ",
+				"し" => "じ",
+				"す" => "ず",
+				"せ" => "ぜ",
+				"そ" => "ぞ",
+				"た" => "だ",
+				"ち" => "ぢ",
+				"つ" => "づ",
+				"て" => "で",
+				"と" => "ど",
+				"は" => "ば",
+				"ひ" => "び",
+				"ふ" => "ぶ",
+				"へ" => "べ",
+				"ほ" => "ぼ",
+				"カ" => "ガ",
+				"キ" => "ギ",
+				"ク" => "グ",
+				"ケ" => "ゲ",
+				"コ" => "ゴ",
+				"サ" => "ザ",
+				"シ" => "ジ",
+				"ス" => "ズ",
+				"セ" => "ゼ",
+				"ソ" => "ゾ",
+				"タ" => "ダ",
+				"チ" => "ヂ",
+				"ツ" => "ヅ",
+				"テ" => "デ",
+				"ト" => "ド",
+				"ハ" => "バ",
+				"ヒ" => "ビ",
+				"フ" => "ブ",
+				"ヘ" => "ベ",
+				"ホ" => "ボ",
+			);
+				
+			$handakuten_chars = array(
+				"は" => "ぱ",
+				"ひ" => "ぴ",
+				"ふ" => "ぷ",
+				"へ" => "ぺ",
+				"ほ" => "ぽ",
+				"ハ" => "パ",
+				"ヒ" => "ピ",
+				"フ" => "プ",
+				"ヘ" => "ペ",
+				"ホ" => "ポ",
+			);
+			
+			print (strtoupper(str_pad(dechex($byte), 2, "0", STR_PAD_LEFT)) .  "=" . $charset[$x] ."<br>");
+			
+			if ($dakuten != null && array_key_exists($charset[$x], $dakuten_chars)) {
+					print (strtoupper(($pre ? $dakuten : '') . str_pad(dechex($byte), 2, "0", STR_PAD_LEFT) . ($pre ? '' : $dakuten)) .  "=" . $dakuten_chars[$charset[$x]] ."<br>");
+			}
+			if ($handakuten != null && array_key_exists($charset[$x], $handakuten_chars)) {
+					print (strtoupper(($pre ? $handakuten : '') . str_pad(dechex($byte), 2, "0", STR_PAD_LEFT) . ($pre ? '' : $handakuten)) .  "=" . $handakuten_chars[$charset[$x]] ."<br>");
+			}
+		}
+	//if no charset is found, print message
+	} else {
+		print "Character not found.";
+	}
+}
---
+++ table_helper.php
@@ -0,0 +1,58 @@
+﻿<?php
+
+include_once("library.php");
+
+//an array of known hex values, one for
+//each character set. EX: array("a5", "af") 
+//$hex = array("09", "14",  "2d", "5b", "64", "90");
+$hex = array(
+"",
+);
+
+//an array of the corresponding known characters,
+//one for each character set. EX: array("０", "あ")
+/*$char = array(
+"０",
+"Ａ",
+"ａ",
+"あ",
+"ぁ",
+"ア",
+"ァ",
+"が",
+"ガ",
+);*/
+$char = array(
+"",
+);
+
+//hex value of the dakuten character (optional)
+//$dakuten = '';
+
+//hex value of the handakuten character (optional)
+//$handakuten = '';
+
+//set true if diacritical marks come before the unvoiced character
+//$pre = true;
+
+//available character sets
+$numerals = get_charset("charsets/numerals.utf");
+$english_upper = get_charset("charsets/english_upper.utf");
+$english_lower = get_charset("charsets/english_lower.utf");
+$hiragana = get_charset("charsets/hiragana.utf");
+$hiragana_small = get_charset("charsets/hiragana_small.utf");
+//$hiragana_small = get_charset("charsets/hiragana_small_alt.utf");
+$hiragana_diacritic = get_charset("charsets/hiragana_diacritic.utf");
+$katakana = get_charset("charsets/katakana.utf");
+$katakana_small = get_charset("charsets/katakana_small.utf");
+$katakana_diacritic = get_charset("charsets/katakana_diacritic.utf");
+//use $current_game for a custom character set you've prepared for a particular game
+//$current_game = get_charset("");
+
+//iterate through each hex/char pair and print the
+//charset in table format based on the supplied value
+for ($x = 0; $x < (count($hex)); $x++) {
+	print_table($char[$x], $hex[$x], $dakuten, $handakuten, $pre);
+	//add a line of whitespace between each set for readability
+	print "<br>";
+}
